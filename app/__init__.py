c4acdec9cf062e72a2132b37571c2aa4a212f4a5
KirwinAl <89887144+KirwinAl@users.noreply.github.com>
2022-03-04 11:00:39 -0500

    Should allow index.html to be accessed at first visit and any callbacks from the other pages

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-    @app.route("/")
+    @app.route("/", "/index.html")
     def index():
         return render_template('index.html')
     @app.route("/gitPage.html")
