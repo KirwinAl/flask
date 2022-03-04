1676af76d8c225b106a76d3aecfdc99908c55040
KirwinAl <89887144+KirwinAl@users.noreply.github.com>
2022-03-04 10:28:28 -0500

    Added route() for the git page, to test if that works

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ def create_app():
     @app.route("/")
     def index():
         return render_template('index.html')
+    @app.route("/git")
     def gitPage():
         return render_template('gitPage.html')
 
